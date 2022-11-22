cdb2a448f0ec25821a50f9468e9732551e32fe32
teddy <tgamiette@kernix.com>
2022-11-22 10:20:08 +0100

    fix docker

--- bin/install.sh
+++ bin/install.sh
@@ -3,15 +3,15 @@
 # build docker compose
 docker-compose up --force-recreate --build -d
 
-docker exec symfony-leboncoin sh -c 'composer install'
-docker exec symfony-leboncoin sh -c 'symfony console assets:install public'
+docker exec symfony_websocket sh -c 'composer install'
+docker exec symfony_websocket sh -c 'symfony console assets:install public'
 
-docker exec symfony-leboncoin sh -c 'symfony console doctrine:database:create --if-not-exists'
-docker exec symfony-leboncoin sh -c 'symfony console cache:clear'
+docker exec symfony_websocket sh -c 'symfony console doctrine:database:create --if-not-exists'
+docker exec symfony_websocket sh -c 'symfony console cache:clear'
 
-docker exec symfony-leboncoin sh -c 'chown -Rf www-data: var/'
-docker exec symfony-leboncoin sh -c 'symfony console d:s:u --force'
-docker exec symfony-leboncoin sh -c 'symfony console d:f:l --append'
+docker exec symfony_websocket sh -c 'chown -Rf www-data: var/'
+docker exec symfony_websocket sh -c 'symfony console d:s:u --force'
+docker exec symfony_websocket sh -c 'symfony console d:f:l --append'
 
-docker exec symfony-leboncoin sh -c 'npm install'
-#docker exec symfony-leboncoin sh -c 'npm run watch'
+#docker exec symfony_websocket sh -c 'npm install'
+#docker exec symfony_websocket sh -c 'npm run watch'
