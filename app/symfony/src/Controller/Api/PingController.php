fd3eeedae8a22e9c5911f3b7b5ad76a3cb6f5ea4
teddy <tgamiette@kernix.com>
2022-11-23 16:09:28 +0100

    add Authenticatord JWT

--- app/symfony/src/Controller/Api/PingController.php
+++ app/symfony/src/Controller/Api/PingController.php
@@ -4,14 +4,16 @@ namespace App\Controller\Api;
 
 use App\Entity\User;
 use App\Service\CookieHelper;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Mercure\HubInterface;
 use Symfony\Component\Mercure\Update;
 use Symfony\Component\Routing\Annotation\Route;
-
+#[Route('/api/ping', name: 'api_ping_')]
 class PingController extends AbstractController
 {
-    #[Route('/ping/{user}', name: 'ping_user', methods: 'POST')]
+    #[Route('/{user}', name: 'ping_user', methods: 'POST')]
+    #[isGranted('ROLE_USER', message: 'Vous devez être connecté pour pouvoir ping un utilisateur')]
     public function pingUser(User $user, HubInterface $hub)
     {
         $update = new Update(
@@ -25,7 +27,7 @@ class PingController extends AbstractController
             ]),
             true
         );
-
+//send to mercure
         $hub->publish($update);
 
         return $this->json([
--- app/symfony/src/Controller/Api/UserController.php
+++ app/symfony/src/Controller/Api/UserController.php
@@ -2,17 +2,31 @@
 
 namespace App\Controller\Api;
 
+use App\Entity\User;
 use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
-class UserController extends AbstractController
-{
-    #[Route('/user-list', name: 'user_list')]
-    public function userList(UserRepository $userRepository)
-    {
-        return $this->json([
-            'users' => $userRepository->findAll()
-        ], 200, [], ['groups' => 'main']);
+#[Route('/api/user', name: 'api_user_')]
+class UserController extends AbstractController {
+    public function __construct(private EntityManagerInterface $em) {
+    }
+
+    #[Route('/list', name: 'user_list')]
+    public function userList() {
+        /**
+         * @var User $user
+         */
+        $user = $this->getUser();
+        $userRepository = $this->em->getRepository(User::class);
+        $userCollection = $userRepository->getAllButMe($user);
+
+        return $this->json(
+            ['users' => $userCollection,
+                'tot'=>"je suis un toto "]
+            , 200,
+            [],
+            ['groups' => 'main']);
     }
 }
