1eb1002470e50af46dc61b19b1264b44a4d62c92
teddy <tgamiette@kernix.com>
2022-11-24 15:53:22 +0100

    fixbug + clean code

--- app/front/src/Hook/useGetUserList.js
+++ app/front/src/Hook/useGetUserList.js
@@ -1,9 +1,13 @@
 export default function useGetUserList() {
-    return function () {
-        return fetch('http://localhost:8245/user-list', {
-            method: 'GET',
-            mode: "cors"
-        })
-            .then(data => data.json())
-    }
+  return function() {
+    return fetch('http://localhost:8245/api/user/list', {
+      method: 'GET',
+      mode: "cors",
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJtZXJjdXJlIjp7InN1YnNjcmliZSI6WyJodHRwczovL2V4YW1wbGUuY29tL3VzZXIvMS97P3RvcGljfSJdLCJwYXlsb2FkIjp7InVzZXJuYW1lIjoiRWxpc2UiLCJ1c2VyaWQiOjF9fX0.UmcD69mVz_kbGPFpt645fVJ0L6O7kZ6Dq5y1nBfoHOY'
+      }
+    })
+      .then(data => data.json())
+  }
 }
